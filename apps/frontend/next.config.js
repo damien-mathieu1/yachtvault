18e4ddc002c799da5156010803f3080da06f91e0
Damien MATHIEU <damien.mathieu@etu.umontpellier.fr>
2025-07-22 03:02:28 +0200

    add posthog

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -18,11 +18,31 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
 
-
   // Configure for monorepo - includes files from the monorepo base two directories up
   outputFileTracingRoot: path.join(process.cwd(), '../../'),
   // Enable standalone output for better deployment
   output: 'standalone',
+
+  // PostHog rewrites to proxy analytic requests
+  async rewrites() {
+    return [
+      {
+        source: '/ingest/static/:path*',
+        destination: 'https://eu-assets.i.posthog.com/static/:path*',
+      },
+      {
+        source: '/ingest/:path*',
+        destination: 'https://eu.i.posthog.com/:path*',
+      },
+      {
+        source: '/ingest/decide',
+        destination: 'https://eu.i.posthog.com/decide',
+      },
+    ];
+  },
+
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
 };
 
 export default withPWA(nextConfig);
